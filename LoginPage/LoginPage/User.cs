5d958c06c1ead44966e19326d34cb49538e3c752
Viacheslav Luzhanskyi <vluzhanskyi@gmail.com>
2016-08-21 17:26:51 +0300

    NewSolution

---
+++ LoginPage/LoginPage/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPage
+{
+    class User
+    {
+        public void ConectToDB(string server, string userID, string password, string initialCatalog)
+        {
+            SqlConnectionStringBuilder connectionString = new SqlConnectionStringBuilder();
+            connectionString.UserID = userID;
+            connectionString.Password = password;
+            connectionString.InitialCatalog = initialCatalog;
+            connectionString.DataSource = server;
+        }
+
+        public void GetDbData(string connectionString, string queryString)
+        {
+            using (SqlConnection connection = new SqlConnection(
+              connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
